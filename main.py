5e43e59a9ba60c899df599b1715c1b989fdb737a
Ivan Ermakov <i.ermakov@bestdoctor.ru>
2022-09-09 00:00:35 +0300

    Json decoder + try/except

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
+# payload = {"name": "User"}
+#
+# response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# print(response.text)
+# print(response.status_code)
+
+
+# response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "Her"})
+# parsed_response_text = response.json()
+#
+# print(parsed_response_text["answer"])
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not a Json format")
