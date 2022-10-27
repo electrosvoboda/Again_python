b79bb7836eeba3161c9a4eceebfaa4835d9a0756
Ivan Ermakov <i.ermakov@bestdoctor.ru>
2022-10-27 23:10:39 +0300

    Update python file. New info on headers

--- types_requests.py
+++ types_requests.py
@@ -1,26 +1,26 @@
 from json.decoder import JSONDecodeError
 import requests
 
-# payload = {"name": "User"}
-#
-# response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-# print(response.text)
-# print(response.status_code)
+payload = {"name": "User"}
+
+response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+print(response.text)
+print(response.status_code)
 
 
-# response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "Her"})
-# parsed_response_text = response.json()
-#
-# print(parsed_response_text["answer"])
+response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "Her"})
+parsed_response_text = response.json()
 
-# response = requests.get("https://playground.learnqa.ru/api/get_text")
-# print(response.text)
-#
-# try:
-#     parsed_response_text = response.json()
-#     print(parsed_response_text)
-# except JSONDecodeError:
-#     print("Response is not a Json format")
+print(parsed_response_text["answer"])
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not a Json format")
 
 # params GET request
 response = requests.get("https://playground.learnqa.ru/api/check_type", params={"param1" : "huyak1"})
@@ -32,13 +32,13 @@ print(response.text)
 
 
 # Status code
-# response = requests.get("https://playground.learnqa.ru/api/get_500")
-# print(response.status_code)
-# print(response.text)
-#
-# response = requests.get("https://playground.learnqa.ru/api/get_50")
-# print(response.status_code)
-# print(response.text)
+response = requests.get("https://playground.learnqa.ru/api/get_500")
+print(response.status_code)
+print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/get_50")
+print(response.status_code)
+print(response.text)
 
 response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
 first_response = response.history[0]
@@ -46,3 +46,11 @@ second_response = response
 
 print(first_response.url)
 print(second_response.url)
+
+
+# Headers
+headers = {"some_header":"123"}
+response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+
+print(response.text)
+print(response.headers)
