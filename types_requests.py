00f8b56070ceaf2efe9f66f304dfd7a2d6191a8c
Ivan Ermakov <i.ermakov@bestdoctor.ru>
2022-10-27 22:56:41 +0300

    New python file with type request

--- types_requests.py
+++ types_requests.py
@@ -30,3 +30,19 @@ print(response.text)
 response = requests.post("https://playground.learnqa.ru/api/check_type", data={"param1" : "huyak1"})
 print(response.text)
 
+
+# Status code
+# response = requests.get("https://playground.learnqa.ru/api/get_500")
+# print(response.status_code)
+# print(response.text)
+#
+# response = requests.get("https://playground.learnqa.ru/api/get_50")
+# print(response.status_code)
+# print(response.text)
+
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+first_response = response.history[0]
+second_response = response
+
+print(first_response.url)
+print(second_response.url)
