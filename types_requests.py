f1794de5bad7a06c72c8e3f341b767d55d872875
Ivan Ermakov <i.ermakov@bestdoctor.ru>
2022-11-24 01:13:53 +0300

    Create first api tests

--- types_requests.py
+++ types_requests.py
@@ -54,3 +54,24 @@ response = requests.get("https://playground.learnqa.ru/api/show_all_headers", he
 
 print(response.text)
 print(response.headers)
+
+headers = {"some_header":"123"}
+response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+
+print(response.text)
+print(response.headers)
+
+# Cookie
+payload = {"login": "secret_login", "password": "secret_pass"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+
+cookie_value = response1.cookies.get('auth_cookie')
+
+cookies = {}
+if cookie_value is not None:
+    cookies.update({'auth_cookie': cookie_value})
+
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies)
+
+
+print(response2.text)
